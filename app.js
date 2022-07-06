e74ab5803e9ff1b6f6c54fef343907374f9cc1c0
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-07-06 21:04:52 +0100

    HTML, CSS styles, and JavaScript for the dropdown menu functionality

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const featuresNavLink = document.querySelector(".navigation__link--features");
 const companyNavLink = document.querySelector(".navigation__link--company");
+const arrowDown1 = document.querySelector(".navigation__slide-icon--1");
+const arrowDown2 = document.querySelector(".navigation__slide-icon--2");
 const dropdown1 = document.querySelector(".dropdown--1");
 const dropdown2 = document.querySelector(".dropdown--2");
 
@@ -7,10 +9,18 @@ function toggleAppear(item) {
   item.classList.toggle("show");
 }
 
+let clicked1 = false;
 featuresNavLink.addEventListener("click", function () {
   toggleAppear(dropdown1);
+  clicked1 = !clicked1;
+  const direction = clicked1 === true ? "up" : "down";
+  arrowDown1.src = `images/icon-arrow-${direction}.svg`;
 });
 
+let clicked2 = false;
 companyNavLink.addEventListener("click", function () {
   toggleAppear(dropdown2);
+  clicked2 = !clicked2;
+  const direction = clicked2 === true ? "up" : "down";
+  arrowDown2.src = `images/icon-arrow-${direction}.svg`;
 });
