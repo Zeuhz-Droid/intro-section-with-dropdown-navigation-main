497f2f02367715cfeb46a2e6467ad1fa873c510a
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-07-07 11:03:32 +0100

    wrote media queries in both CSS and Javascript for the dropdown section, and completed the code generally

--- app.js
+++ app.js
@@ -1,26 +1,76 @@
 const featuresNavLink = document.querySelector(".navigation__link--features");
 const companyNavLink = document.querySelector(".navigation__link--company");
-const arrowDown1 = document.querySelector(".navigation__slide-icon--1");
-const arrowDown2 = document.querySelector(".navigation__slide-icon--2");
-const dropdown1 = document.querySelector(".dropdown--1");
-const dropdown2 = document.querySelector(".dropdown--2");
+const arrowFeatures = document.querySelector(
+  ".navigation__slide-icon--features"
+);
+const navigationItemFeatures = document.querySelector(".navigation__item");
+const navigationItemCompany = document.querySelector(
+  ".navigation__item:nth-child(2)"
+);
+const arrowCompany = document.querySelector(".navigation__slide-icon--company");
+const dropdownFeatures = document.querySelector(".dropdown-features");
+const dropdownCompany = document.querySelector(".dropdown-company");
 
 function toggleAppear(item) {
   item.classList.toggle("show");
 }
 
-let clicked1 = false;
-featuresNavLink.addEventListener("click", function () {
-  toggleAppear(dropdown1);
-  clicked1 = !clicked1;
-  const direction = clicked1 === true ? "up" : "down";
-  arrowDown1.src = `images/icon-arrow-${direction}.svg`;
-});
+function addShow() {
+  dropdownFeatures.classList.add("show");
+  dropdownCompany.classList.add("show");
+}
 
-let clicked2 = false;
-companyNavLink.addEventListener("click", function () {
-  toggleAppear(dropdown2);
-  clicked2 = !clicked2;
-  const direction = clicked2 === true ? "up" : "down";
-  arrowDown2.src = `images/icon-arrow-${direction}.svg`;
-});
+const mediaQuery = window.matchMedia("(max-width: 37.5em)");
+function handleScreenChange(e) {
+  if (e.matches) {
+    let clicked1 = false;
+    let clicked2 = false;
+
+    featuresNavLink.addEventListener("click", function () {
+      toggleAppear(dropdownFeatures);
+      clicked1 = !clicked1;
+      arrowFeatures.src = `images/icon-arrow-${clicked1 ? "up" : "down"}.svg`;
+    });
+
+    companyNavLink.addEventListener("click", function () {
+      toggleAppear(dropdownCompany);
+      clicked2 = !clicked2;
+      arrowCompany.src = `images/icon-arrow-${clicked2 ? "up" : "down"}.svg`;
+    });
+  }
+}
+mediaQuery.addListener(handleScreenChange);
+handleScreenChange(mediaQuery);
+
+const mediaQuery2 = window.matchMedia("(min-width: 37.5em)");
+function handleScreenChange2(e) {
+  if (e.matches) {
+    console.log("It works");
+
+    navigationItemFeatures.addEventListener("mouseenter", function () {
+      dropdownFeatures.classList.remove("show");
+      arrowFeatures.src = `images/icon-arrow-up.svg`;
+    });
+
+    dropdownFeatures.addEventListener("mouseleave", function () {
+      setTimeout(() => {
+        dropdownFeatures.classList.add("show");
+        arrowFeatures.src = `images/icon-arrow-down.svg`;
+      }, 200);
+    });
+
+    navigationItemCompany.addEventListener("mouseenter", function () {
+      dropdownCompany.classList.remove("show");
+      arrowCompany.src = `images/icon-arrow-up.svg`;
+    });
+
+    dropdownCompany.addEventListener("mouseleave", function () {
+      setTimeout(() => {
+        dropdownCompany.classList.add("show");
+        arrowCompany.src = `images/icon-arrow-down.svg`;
+      }, 200);
+    });
+  }
+}
+mediaQuery2.addListener(handleScreenChange2);
+handleScreenChange2(mediaQuery2);
